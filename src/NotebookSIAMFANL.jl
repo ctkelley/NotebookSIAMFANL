37c8af13808a66cd39c4c6194906fb76e74c0f04
Tim Kelley <ctk@ncsu.edu>
2020-06-09 15:56:52 -0400

    cleanup

--- src/NotebookSIAMFANL.jl
+++ src/NotebookSIAMFANL.jl
@@ -11,9 +11,11 @@ using SIAMFANLEquations.TestProblems
 export atan_test
 export ftan_test
 export ptc_scalar_example
+export fig1.1
 
 include("ScalarEquations/atan_test.jl")
 include("ScalarEquations/ftan_test.jl")
 include("ScalarEquations/ptc_scalar_example.jl")
+include("ScalarEquations/fig1.1.jl")
 
 end # module
