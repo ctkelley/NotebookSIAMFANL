d29e9bd4439a40dea1bcf326097a5cc7f9ce0fed
Tim Kelley <ctk@ncsu.edu>
2020-06-11 15:14:46 -0400

    Cleanup for June 11

--- src/NotebookSIAMFANL.jl
+++ src/NotebookSIAMFANL.jl
@@ -11,6 +11,7 @@ using SIAMFANLEquations.TestProblems
 export atan_test
 export ftan_test
 export ptc_scalar_example
+export tab1dot1
 export fig1dot1
 export fig1dot2
 export fig1dot3
@@ -20,6 +21,7 @@ include("Chapter1/fig1dot1.jl")
 include("Chapter1/fig1dot2.jl")
 include("Chapter1/fig1dot3.jl")
 include("Chapter1/fig1dot4.jl")
+include("Chapter1/tab1dot1.jl")
 include("Chapter1/ptc_scalar_example.jl")
 
 end # module
