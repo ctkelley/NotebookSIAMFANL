87e11115f68b28c259f37e934c7c525fcadad101
Tim Kelley <ctk@ncsu.edu>
2020-06-11 10:10:12 -0400

    Figures

--- src/Chapter1/fig1dot3.jl
+++ src/Chapter1/fig1dot3.jl
@@ -1,14 +1,14 @@
 """
-ftan_test()
+fig1dot3()
 
 This is the Julia code for the tan(x) = x example.
 
-This code makes Figures 1.2 and 1.3 in the print book.
+This code makes Figure in the print book.
 
 See the notebook.
 
 """
-function ftan_test()
+function fig1dot3()
     nnout=nsolsc(ftanx,4.5; maxit=14, rtol=1.e-17, atol=1.e-17)
     lnn=length(nnout.history)
     nncounter=0:lnn-1
@@ -18,12 +18,6 @@ function ftan_test()
     scout=nsolsc(ftanx,4.5; maxit=6, rtol=1.e-17, atol=1.e-17, solver="secant")
     lns=length(scout.history)
     sccounter=0:lns-1
-figure(1)
- semilogy(0:5, nnout.history[1:6],"k-")
-ylabel("Log Absolute Nonlinear Residual")
-xlabel("Nonlinear Iterations")
-plt.text(4.1, 2.e-15,"Stagnation")
-figure(2)
 semilogy(nncounter, nnout.history,"k-",
 nccounter, chout.history,"k--",
 sccounter, scout.history,"k-."
@@ -31,7 +25,7 @@ sccounter, scout.history,"k-."
 ylabel("Log Absolute Nonlinear Residual")
 xlabel("Nonlinear Iterations")
 legend(("Newton","Chord","Secant"))
-return nnout
+#return nnout
 end
 
 #function ftanx(x)
--- src/NotebookSIAMFANL.jl
+++ src/NotebookSIAMFANL.jl
@@ -13,12 +13,14 @@ export ftan_test
 export ptc_scalar_example
 export fig1dot1
 export fig1dot2
+export fig1dot3
 export fig1dot4
 
 include("Chapter1/ftan_test.jl")
 include("Chapter1/ptc_scalar_example.jl")
 include("Chapter1/fig1dot1.jl")
 include("Chapter1/fig1dot2.jl")
+include("Chapter1/fig1dot3.jl")
 include("Chapter1/fig1dot4.jl")
 
 end # module
