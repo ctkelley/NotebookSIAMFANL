01978a8eea23357f9677b69848676faa036c0ee4
Tim Kelley <ctk@ncsu.edu>
2020-06-15 14:56:15 -0400

    adding stuff

--- src/Chapter1/fig1dot5.jl
+++ src/Chapter1/fig1dot5.jl
@@ -1,7 +1,7 @@
 """
-fig1dot4()
+fig1dot5()
 
-This is the Julia code for Figures 1.1 and 1.4 of the 
+This is the Julia code for Figure 1.5 of the 
 Green book. 
 
 I documented the solver in nsolsc.jl. The challenge here
@@ -17,33 +17,29 @@ and fpatan(x) = 1.0/(1.0+x^2)
 is in this .jl file. 
 
 """
-function fig1dot4()
+function fig1dot5()
 #
-# Figure 1.4. 
+# Figure 1.5. 
 #
         global_hist = nsolsc(
             atan,
             10.0;
-            fp = fpatan, rtol = 1.e-8, atol = 1.e-10, maxit = 20,
-            armfix=true
+            fp = fpatan, rtol = 1.e-8, atol = 1.e-10, maxit = 20
         )
-figure(3)
         rval = global_hist.history
         itc=length(rval)
         ival = 0:itc-1
         semilogy(ival, abs.(rval), "k-")
-        plt.text(ival[2], abs(rval[2] * 5), "3")
+        plt.text(ival[2], abs(rval[2] * 5), "2")
         plot(ival[2], abs(rval[2]), "ko")
-        plt.text(ival[3], abs(rval[3] * 5), "3")
+        plt.text(ival[3], abs(rval[3] * 5), "1")
         plot(ival[3], abs(rval[3]), "ko")
-        plt.text(ival[4], abs(rval[4] * 5), "2")
+        plt.text(ival[4], abs(rval[4] * 5), "1")
         plot(ival[4], abs(rval[4]), "ko")
-        plt.text(ival[5], abs(rval[5] * 5), "2")
-        plot(ival[5], abs(rval[5]), "ko")
-        ylim(1.e-11, 1000)
+        ylim(1.e-16, 1000)
         xlim(0, 12)
         ylabel("Absolute Nonlinear Residual")
         xlabel("Nonlinear iterations")
-        title("Figure 1.4 from print book")
+#        title("Figure 1.5 from print book")
     end
 
--- src/NotebookSIAMFANL.jl
+++ src/NotebookSIAMFANL.jl
@@ -15,11 +15,13 @@ export fig1dot1
 export fig1dot2
 export fig1dot3
 export fig1dot4
+export fig1dot5
 
 include("Chapter1/fig1dot1.jl")
 include("Chapter1/fig1dot2.jl")
 include("Chapter1/fig1dot3.jl")
 include("Chapter1/fig1dot4.jl")
+include("Chapter1/fig1dot5.jl")
 include("Chapter1/tab1dot1.jl")
 include("Chapter1/ptc_scalar_example.jl")
 
