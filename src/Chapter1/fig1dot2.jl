bb5568ea93ea5e73bb8d76fd4f0dd3bf0dedd363
Tim Kelley <ctk@ncsu.edu>
2020-07-25 09:20:43 -0400

    unfixing

--- src/Chapter1/fig1dot2.jl
+++ src/Chapter1/fig1dot2.jl
@@ -12,11 +12,11 @@ function fig1dot2()
     nnout=nsolsc(ftanx,4.5; maxit=14, rtol=1.e-17, atol=1.e-17, printerr=false)
     lnn=length(nnout.history)
     nncounter=0:lnn-1
-    chout=nsolsc(ftanx,4.5; maxit=14, rtol=1.e-17, atol=1.e-17, solver=:chord,
+    chout=nsolsc(ftanx,4.5; maxit=14, rtol=1.e-17, atol=1.e-17, solver="chord",
                  printerr=false)
     lnc=length(chout.history)
     nccounter=0:lnc-1
-    scout=nsolsc(ftanx,4.5; maxit=6, rtol=1.e-17, atol=1.e-17, solver=:secant,
+    scout=nsolsc(ftanx,4.5; maxit=6, rtol=1.e-17, atol=1.e-17, solver="secant",
                 printerr=false)
     lns=length(scout.history)
     sccounter=0:lns-1
