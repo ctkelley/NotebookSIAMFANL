08aac82b6ff546d3a93e6d23840a5140146111f0
Tim Kelley <ctk@ncsu.edu>
2020-09-23 09:04:39 -0400

    Example submodule now drives more figues and plots

--- src/Chapter2/fig2dot3.jl
+++ src/Chapter2/fig2dot3.jl
@@ -3,7 +3,7 @@ function fig2dot3(n = 801, T = Float64)
 #   Solve the problem and make the figure. The good stuff is
 #   in bvp_solve
 #
-    figout = bvp_solve(n, T)
+    figout = BVP_solve(n, T; bfact=qr!)
     bvpout = figout.bvpout
     tv = figout.tv
     vlabel = L"$v$"
@@ -15,36 +15,4 @@ function fig2dot3(n = 801, T = Float64)
     axis([0, 20, -1.5, 2.5])
     xticks(collect(0:2:20))
     title("Fig 2.3 in print book")
-end
-#
-"""
-bvp_solve
-
-Solve the boundary value problem to make Figure 2.3.
-"""
-function bvp_solve(n = 801, T = Float64)
-    # set it up
-    bdata = bvpinit(n, T)
-    #
-    U0 = zeros(2n)
-    FV = zeros(2n)
-    FPV = bdata.JacS
-    #
-    # tv = 0:h:20 is the spatial mesh
-    #
-    tv = bdata.tv
-    #
-    # Build the initial iterate
-    #
-    sv = -.1 * tv .* tv
-    v = exp.(sv)
-    vp = -.2 * tv .* v
-    U0[1:2:2n-1] = v
-    U0[2:2:2n] = vp
-    #
-    # Call the solver. Notice I'm putting my analytic Jacobian evaluation
-    # in there.
-    #
-    bvpout = nsold(Fbvp!, U0, FV, FPV, Jbvp!; rtol = 1.e-10, pdata = bdata)
-    return (bvpout = bvpout, tv = tv)
 end
