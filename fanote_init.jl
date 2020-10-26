6a49e0183b42b9c13ce48b3bd22a3542d3206d6e
Tim Kelley <ctk@ncsu.edu>
2020-10-26 09:31:06 -0400

    update

--- fanote_init.jl
+++ fanote_init.jl
@@ -2,6 +2,7 @@ using SIAMFANLEquations
 using SIAMFANLEquations.TestProblems
 using SIAMFANLEquations.Examples
 using LinearAlgebra
+using BenchmarkTools
 using PyPlot
 push!(LOAD_PATH,"./src")
 using NotebookSIAMFANL
