6df76a10a68cf1e0a07b0ee585a1742922781255
Tim Kelley <ctk@ncsu.edu>
2021-11-27 16:46:22 -0500

    Updates with more fixes for 1.6.4 problem

--- fanote_init.jl
+++ fanote_init.jl
@@ -2,6 +2,7 @@ function vcheck()
 if (VERSION==v"1.6.4")
    println("Bug in Julia 1.6.4")
    println("See: https://discourse.julialang.org/t/problems-in-1-6-4-with-ijulia-downgrade-to-1-6-3/72129/10")
+   println("Your best bet is to use 1.6.3")
 error("Notebook will not work with Julia v1.6.4. ")
 end
 end
