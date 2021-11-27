623fe0b75397246565f3e17adc6ce4c2acd1d302
C. T. (Tim) Kelley <tim_kelley@ncsu.edu>
2021-11-27 12:37:05 -0500

    Test for 1.6.4 in the init.

--- fanote_init.jl
+++ fanote_init.jl
@@ -1,3 +1,6 @@
+VERSION==v"1.6.4" && error("Notebook will not work with 1.6.4. See
+https://discourse.julialang.org/t/problems-in-1-6-4-with-ijulia-downgrade-to-1-6-3/72129/12")
+using NotebookSIAMFANL
 using SIAMFANLEquations
 using SIAMFANLEquations.TestProblems
 using SIAMFANLEquations.Examples
