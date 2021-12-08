7bcfebc3ac78ef4351fd02b136832a06313202f7
Tim Kelley <ctk@ncsu.edu>
2021-12-08 13:56:35 -0500

    Checking for MKL with 1.7

--- fanote_init.jl
+++ fanote_init.jl
@@ -1,16 +1,24 @@
+using LinearAlgebra
 function vcheck()
-if (VERSION==v"1.6.4") || (VERSION==v"1.7.0") 
-   println("Bug in Julia 1.6.4")
+if (VERSION==v"1.7.0")
+mklok = any(contains("mkl"), getfield.(BLAS.get_config().loaded_libs, :libname))
+else
+mklok = false
+end
+blasproblem = ~mklok || (VERSION==v"1.6.4")
+#if (VERSION==v"1.6.4") || (VERSION==v"1.7.0") 
+#if (VERSION==v"1.6.4") 
+if blasproblem
+   println("Bug in Julia 1.6.4 and 1.7.0")
    println("See: https://discourse.julialang.org/t/problems-in-1-6-4-with-ijulia-downgrade-to-1-6-3/72129/10")
-   println("Your best bet is to use 1.6.3")
-error("Notebook will not work with Julia v1.6.4 or v1.7.0. ")
+   println("Your best bet is to use 1.6.3 or use MKL with 1.7.0")
+error("OpenBLAS + Notebook will not work with Julia v1.6.4 or v1.7.0. ")
 end
 end
 vcheck()
 using SIAMFANLEquations
 using SIAMFANLEquations.TestProblems
 using SIAMFANLEquations.Examples
-using LinearAlgebra
 using BandedMatrices
 using BenchmarkTools
 using PyPlot
