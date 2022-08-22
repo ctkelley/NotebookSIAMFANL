c9d9fa8e930342c8c169fb1afc50ccbd662a2622
Tim Kelley <ctk@ncsu.edu>
2022-08-21 20:31:53 -0400

    Fixing 2nd round of proofs + test on M2 with 1.8.0

--- fanote_init.jl
+++ fanote_init.jl
@@ -10,9 +10,6 @@ mklok = false
 end
 blasproblem = ~mklok && (VERSION==v"1.6.4")
 if blasproblem
-   println("Bug in Julia 1.6.4 and 1.7.0")
-   println("See: https://discourse.julialang.org/t/problems-in-1-6-4-with-ijulia-downgrade-to-1-6-3/72129/10")
-   println("Your best bet is to use 1.6.5 (LTS) or 1.7.1 (or higher)")
 error("OpenBLAS + Notebook will not work with Julia v1.6.4 or v1.7.0. ")
 end
 end
